1349d8fe0ec2046f6d236efa01e84fd5a06ce2ee
Igor Goltsov <riversy@gmail.com>
2016-09-29 12:18:07 +0300

    Gulp should build the source before watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,15 @@
 let babel = require('gulp-babel');
 let gulp = require('gulp');
 
+gulp.task('default', ['build']);
+
 gulp.task('build', function() {
     return gulp.src('src/**/*.js')
         .pipe(babel({ presets: ['es2015'] }))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', ['build'], function() {
     return gulp.watch([
         './src/**/*.js',
     ], ['build']);
