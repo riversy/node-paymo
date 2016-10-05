aeaf536a1a08be99720d5a5b40e8035cbb11038a
Igor Goltsov <riversy@gmail.com>
2016-10-05 23:51:52 +0300

    Add # linter into build process

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,27 @@
 let babel = require('gulp-babel');
 let gulp = require('gulp');
+let eslint = require('gulp-eslint');
+
+const PATH_SRC = ['./src/**/*.js'];
+const PATH_DIST = ['./dist'];
 
 gulp.task('default', ['build']);
 
-gulp.task('build', function() {
-    return gulp.src('src/**/*.js')
+gulp.task('lint', function(){
+    return gulp.src(PATH_SRC)
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('build', ['lint'], function() {
+    return gulp.src(PATH_SRC)
         .pipe(babel({ presets: ['es2015'] }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(PATH_DIST));
 });
 
 gulp.task('watch', ['build'], function() {
     return gulp.watch([
-        './src/**/*.js',
+        PATH_DIST,
     ], ['build']);
 });
