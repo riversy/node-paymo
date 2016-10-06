206b4c47d59065d5160f3aa34ba97271d04bd5bd
Igor Goltsov <riversy@gmail.com>
2016-10-06 09:09:41 +0300

    Update # Refactor constant names in Gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,20 +2,20 @@ let babel = require('gulp-babel');
 let gulp = require('gulp');
 let eslint = require('gulp-eslint');
 
-const PATH_SRC = ['./src/**/*.js'];
+const PATH_SOURCE = ['./src/**/*.js'];
 const PATH_DIST = 'dist';
 
 gulp.task('default', ['build']);
 
 gulp.task('lint', function(){
-    return gulp.src(PATH_SRC)
+    return gulp.src(PATH_SOURCE)
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
 
 gulp.task('build', ['lint'], function() {
-    return gulp.src(PATH_SRC)
+    return gulp.src(PATH_SOURCE)
         .pipe(babel({ presets: ['es2015'] }))
         .pipe(gulp.dest(PATH_DIST));
 });
