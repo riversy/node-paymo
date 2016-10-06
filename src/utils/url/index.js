c3e6cff787c963f6424e51b141874f149f342c72
Igor Goltsov <riversy@gmail.com>
2016-10-06 09:53:43 +0300

    Add # Basic implementation of URL.builder function (which is covered by tests)

--- src/utils/url/index.js
+++ src/utils/url/index.js
@@ -2,16 +2,26 @@ const ENDPOINT_URL = "https://app.paymoapp.com/api";
 
 export default class UrlBuilder {
 
-    build(entity, {
-        id = false,
-        filters = false,
-        fields = false
-    }) {
+    build(entity, options = {}) {
+
+        let {
+            id = false,
+            filters = false,
+            include = false
+        } = options;
+
+        if (!entity){
+            throw new Error("Entity name is required.");
+        }
+
+        let url = `${ENDPOINT_URL}/${entity}`;
+        if (id){
+            url = `${url}/${id}`
+        }
 
 
 
 
-
-
+        return url;
     }
 }
--- src/utils/url/index.spec.js
+++ src/utils/url/index.spec.js
@@ -1,12 +1,25 @@
 
-import {expect} from 'chai';
+import {assert, expect} from 'chai';
+import Url from './index';
+let url = new Url();
 
-describe('My first test', function(){
-    it('Should be great', function(){
-        expect(1 + 2).to.equal(3);
+describe("URL Builder", function(){
+
+    it("should generate exception if entity name isn't defined", function(){
+        expect(url.build).to.throw("Entity name is required.");
     });
-});
 
+    it("should build List URLs", function(){
+        expect(url.build('clients')).to.be.equal('https://app.paymoapp.com/api/clients');
+    });
+
+    it("should build Get and Delete URLs", function(){
+        let id = 10;
+        expect(url.build('clients', {id})).to.be.equal('https://app.paymoapp.com/api/clients/10');
+    });
+
+    
+});
 
 
 
